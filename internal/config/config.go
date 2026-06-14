9acf27490008b7c760a593fa0c350746a1bbc5fc
vijayvenkatj <vijayvenkatj@gmail.com>
2026-06-15 00:45:03 +0530

    chore(setup): added cobra and viper

---
+++ internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+// Config stores all configuration of the application.
+// The values are read by viper from a config file or environment variables.
+type Config struct {
+	DatabaseURL string `mapstructure:"DATABASE_URL"`
+	LogLevel    string `mapstructure:"LOG_LEVEL"`
+}
+
+// LoadConfig reads configuration from file or environment variables.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return
+		}
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
